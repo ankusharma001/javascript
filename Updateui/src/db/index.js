7e5d7385507bd509cb363b1636d66846c7f605ca
Ankush Sharma <sharmankush004a@gmail.com>
2025-04-21 17:22:27 +0530

    connect to db

---
+++ Updateui/src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    // Connect to MongoDB
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Connected to MongoDB successfully");
+    console.log(`MONGODB HOST: ${connectionInstance.connection.host}`);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+export default connectDB;
---
+++ Updateui/src/index.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express";
+const app = express()
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+  path: './.env'
+});
+
+connectDB()
+
+
+// (async () => {
+//   try{
+//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//     app.on("error", (error)=>{
+//       console.log("Error: ", error);
+//       throw error
+      
+//     })
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//     console.log("Connected to MongoDB successfully");
+
+//   }catch(err){
+//     console.log("ERROR : ",  err)
+//     throw err
+//   }
+// })()
