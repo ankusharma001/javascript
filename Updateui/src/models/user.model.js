08f16a8da6893e1da59ffa300e55cfe4621be6f9
Ankush Sharma <sharmankush004a@gmail.com>
2025-04-23 15:54:19 +0530

    user and video model

---
+++ Updateui/src/models/user.model.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+import { transformWithEsbuild } from "vite";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userschema =  new monggose.schema({
+  username:{
+    type :String,
+    required : true,
+    unique:true,
+    lowercase:true,
+    trim : true,
+    index:true,
+
+  },
+  
+  email:{
+    type :String,
+    required : true,
+    unique:true,
+    lowercase:true,
+    trim : true
+  },
+  fullname:{
+    type :String,
+    required : true,
+   
+    lowercase:true,
+    trim : true,
+    index:true
+  },
+  password:{
+    type: String,
+    required:[true,'Passrod is required'],
+  },
+  avatar:{
+    type :String,
+    required : true,
+
+  },
+  coverIMAGE:{
+    type :String,
+    required : false,
+
+  },
+  watchHistory:[{
+    type: schema.Types.ObjectId,
+    ref: "WatchHistory",
+  }],
+  refreshToken:{
+    type: String,
+    required: false,
+  },
+
+
+},{timestamps:true})
+
+
+userschema.pre("save", async function(next){
+  if (!this.IsModified("password")){
+    return next()
+  }
+  this.password = bcrypt.hashSync(this.password, 10)
+  next()
+})
+
+userschema.methods.isPasswordcorrect  = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+userschema.methods.generateAccessToken = function(){
+  return jwt.sign({
+    id: this._id,
+    email: this.emial,
+    username : this.username,
+    fullname : this.fullname
+  }, process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  })
+}
+
+
+userschema.methods.generateRefreashToken   = function(){
+  return jwt.sign({
+    id: this._id,
+    email: this.emial,
+    username : this.username,
+    fullname : this.fullname
+  }, process.env.Refresh_TOKEN_SECRET,{
+    expiresIn: process.env.Refresh_TOKEN_EXPIRY
+  })
+}
+
+export const User = mongoose.model("User", userschema);
---
+++ Updateui/src/models/video.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import User from "./user.model.js";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const userschema =  new monggose.schema({
+
+  videoofile:{
+    type :String,
+    required : true
+
+
+  },
+  thumbnail:{
+    type :String,
+    required : true
+  },
+  title:{
+    type :String,
+    required : true,
+
+  
+
+  },
+  description:{
+    type :String,
+    required : true,
+   
+  },
+
+  duration:{
+    type :Number,
+    required : true,
+  },
+  views :{
+    type :Number,
+    default : 0
+  },
+  isPublished:{
+    type :Boolean,
+    default : false
+  },
+  owner:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+},{timesstamps:true})
+
+Videoschema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", userschema)
